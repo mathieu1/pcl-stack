8a0862b4ff78fc60dfb93867aa10813d49cf87bd
Sam Gillingham <gillingham.sam@gmail.com>
2016-06-27 14:05:30 +1000

    removed setting of LDFLAGS since this is done in toolchain

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,6 @@
 source activate "${CONDA_DEFAULT_ENV}"
 
 if [ $(uname) == Darwin ]; then
-    export LDFLAGS="-headerpad_max_install_names"
     OPTS="--enable-rpath"
     export CXX="${CXX} -stdlib=libc++"
 else
